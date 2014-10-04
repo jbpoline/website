8184da1485417807745012d80716b7a5f8a14e8c
jbpoline <jbpoline@gmail.com>
2014-10-04 16:55:42 -0700

    Add test content

---
+++ content/test.md
@@ -0,0 +1,7 @@
+title: This my title 
+author: Jean-Baptiste Poline
+date: 2014-10-01
+slug: testslug
+
+Test first
+
--- pelicanconf.py
+++ pelicanconf.py
@@ -11,6 +11,20 @@ PATH = 'content'
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
+THEME = u'_theme'
+
+PLUGIN_PATHS = ['_plugins']
+PLUGINS = [#'liquid_tags.img', #'liquid_tags.video',
+           #'liquid_tags.youtube', 'liquid_tags.vimeo',
+           'liquid_tags.include_code', 'liquid_tags.notebook']
+
+## Title menu options (this isn't necessary, but I wanted to have more control)
+DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_TAGS_ON_SIDEBAR = False
+
+MENUITEMS = [('publications', 'http://scholar.google.com/citations?user=BU7Zdi4AAAAJ&hl=en')]
+
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -18,16 +32,17 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#         ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+CC_LICENSE = "CC-BY-NC-SA"
